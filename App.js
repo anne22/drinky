beaf0888414b51d07a8cf49bb3abbffb78eb6299
Anne <leuschneranne@gmail.com>
2024-09-11 15:45:36 +0200

    feat: Add modal with buttons for sober and drank options
    Adds a modal with buttons for users to select if their day was sober or drank.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { Calendar, toDateId, CalendarTheme } from "@marceloterreiro/flash-calendar";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Pressable } from 'react-native';
 import React, { useState } from 'react';
 
 const today = toDateId(new Date());
@@ -9,32 +9,62 @@ let selectedDates = new Set();
 
 export default function App() {
   const [dates, setDates] = useState(new Set());
-
+  const [currentdate, setcurrentdate] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false); 
+  
   function datesToRanges(dates) {
     return Array.from(selectedDates).map((date) => {
       return { startId: date, endId: date };
     });
   }
 
-  function handleClick(date) {
-    if (selectedDates.has(date)) {
-      selectedDates.delete(date);
-    } else {
-      selectedDates.add(date);
+  function onSober() {
+    setShowConfirmation(false);
+    if (!selectedDates.has(currentdate)) {
+   
+      selectedDates.add(currentdate);
     }
     setDates(new Set(selectedDates));
   }
-
+  
+  function handleClick(date) {
+    setShowConfirmation(true);
+    setcurrentdate(date);
+    
+  }
+  function onDrank() {
+    setShowConfirmation(false);
+    if (selectedDates.has(currentdate)) {
+      selectedDates.delete(currentdate);
+    }
+    setDates(new Set(selectedDates));
+  }
   let dateRanges = datesToRanges(dates);
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container}> 
+    
+      <Modal animationType="slide" transparent={true} visible={showConfirmation} >
+      <View style={styles.centered}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>What type of day is it?</Text>
+          <Pressable onPress={onSober}>
+          <Text style={styles.soberbutton}>Sober</Text>
+          </Pressable>
+          <Pressable onPress={onDrank}>
+          <Text style={styles.drankbutton}>Drank</Text>
+          </Pressable>
+        </View>
+      </View>
+      </Modal>
+
       <Calendar
         calendarMonthId={today}
         theme={linearTheme}
         calendarActiveDateRanges={dateRanges}
         onCalendarDayPress={handleClick}
         />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -91,6 +121,35 @@ let linearTheme = {
 let styles = StyleSheet.create({
   container: {
     backgroundColor: '#41494a',
+    height: '100%',
     paddingTop: 80,
+  },
+  centered: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  modal: {
+    backgroundColor: '#41494a',
+
+    paddingTop: 80,
+  },
+  soberbutton: {
+    backgroundColor: '#42d6b1', fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  drankbutton: {
+    backgroundColor: '#f0438b',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
   },
 });
